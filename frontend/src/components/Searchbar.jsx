713a8a9d6e241d346144190bc103fcc87ec9e100
Marc LANTOL <marc.lantol@gmail.com>
2022-04-13 15:35:35 +0200

    Title movies are displayed in Home

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,14 +1,16 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import axios from "axios";
 
 function Searchbar() {
+  const [moviesData, setMoviesData] = useState([]);
+
   useEffect(() => {
     axios
       .get(
         "https://api.themoviedb.org/3/search/movie?api_key=20d0a760d82811eb01a3f02b31edc400&query=star&language=en-US&page=1&include_adult=false"
       )
       // eslint-disable-next-line no-restricted-syntax
-      .then((response) => console.log(response.data.results));
+      .then((response) => setMoviesData(response.data.results));
   }, []);
   return (
     <div className="seachbar-component">
@@ -19,6 +21,11 @@ function Searchbar() {
           placeholder="Nom de votre film ?"
         />
       </form>
+      <div className="movie-list">
+        {moviesData.map((movie) => (
+          <h3>{movie.title}</h3>
+        ))}
+      </div>
     </div>
   );
 }
