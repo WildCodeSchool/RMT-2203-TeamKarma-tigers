e38ab97a03dea26e35cd18c772fb6b08c59b7883
Marc LANTOL <marc.lantol@gmail.com>
2022-04-13 15:26:50 +0200

    First axios call + fix package.json axios

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import { React, useEffect } from "react";
+import axios from "axios";
 
 function Searchbar() {
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/search/movie?api_key=20d0a760d82811eb01a3f02b31edc400&query=star&language=en-US&page=1&include_adult=false"
+      )
+      // eslint-disable-next-line no-restricted-syntax
+      .then((response) => console.log(response.data.results));
+  }, []);
   return (
     <div className="seachbar-component">
       <form>
-        <input type="text" placeholder="Nom de votre film ?" />
+        <input
+          className="movie-input"
+          type="text"
+          placeholder="Nom de votre film ?"
+        />
       </form>
     </div>
   );
