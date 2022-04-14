9a80433593ee9633de4ee5fec1dee6814becf1b0
Marc LANTOL <marc.lantol@gmail.com>
2022-04-14 11:59:23 +0200

    Fix movie list in searchbar to disapear when there is no input

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -11,25 +11,28 @@ function Searchbar() {
       .get(
         `https://api.themoviedb.org/3/search/movie?api_key=20d0a760d82811eb01a3f02b31edc400&query=${search}&language=en-US&page=1&include_adult=false`
       )
-      .then((response) => setMoviesData(response.data.results));
+      .then((response) => [setMoviesData(response.data.results)]);
   }, [search]);
+
   return (
     <div className="seachbar-component">
       <form>
         <input
           className="movie-input"
           type="text"
-          placeholder="Nom de votre film ?"
+          placeholder="   Nom de votre film ?"
           onChange={(e) => setSearch(e.target.value)}
         />
       </form>
-      <div className="movie-list">
-        {moviesData.map((movie) => (
-          <a className="movie-link" href="/">
-            {movie.title}
-          </a>
-        ))}
-      </div>
+      {moviesData.length !== 0 && search !== "" && (
+        <div className="movie-list">
+          {moviesData.map((movie) => (
+            <a className="movie-link" href="/">
+              {movie.title}
+            </a>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
