fdfc1f8bee4743ad28a85e25d2690c6041d582b4
kaitolight <alexandre.pillias@gmail.com>
2022-04-14 15:58:23 +0200

    did requested changes

--- frontend/src/components/CardDetailSynopsis.jsx
+++ frontend/src/components/CardDetailSynopsis.jsx
@@ -7,32 +7,30 @@ export default function CardDetailSynopsis() {
       <div className="synopsis-container">
         <h3>SYNOPSIS</h3>
         <ReadMore>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-          laboriosam quas facilis maxime quibusdam earum harum iusto, quisquam
-          aliquam voluptates praesentium ipsum debitis a nobis excepturi iure
-          quia placeat Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Perspiciatis laudantium dignissimos sunt consequatur obcaecati ad,
-          ipsam aliquam aliquid culpa. Saepe dolorem cum laudantium eaque
-          facilis provident asperiores. Molestias, molestiae saepe.
+          A ticking-time-bomb insomniac and a slippery soap salesman channel
+          primal male aggression into a shocking new form of therapy. Their
+          concept catches on, with underground &quotfight clubs&quot forming in
+          every town, until an eccentric gets in the way and ignites an
+          out-of-control spiral toward oblivion.
         </ReadMore>
       </div>
       <div className="utility-container">
         <div className="parent">
           <h3 className="release-date">
             {" "}
-            RELEASE DATE <br /> <h5>CONTENT</h5>
+            RELEASE DATE <br /> <h5>1999-10-15</h5>
           </h3>
           <h3 className="genre">
             {" "}
-            GENRE <br /> <h5>CONTENT</h5>
+            GENRE <br /> <h5>Drama</h5>
           </h3>
           <h3 className="popularity">
             {" "}
-            POPULARITY <br /> <h5>CONTENT</h5>
+            POPULARITY <br /> <h5>52.214</h5>
           </h3>
           <h3 className="revenue">
             {" "}
-            REVENUE <br /> <h5>CONTENT</h5>
+            REVENUE <br /> <h5>100853753</h5>
           </h3>
         </div>
       </div>
