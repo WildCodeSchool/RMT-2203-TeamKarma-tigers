94b67cde78b6290f6c1a945f0cbb93ab52fc3b3b
Julie Combeau <combeau.julie@gmail.com>
2022-04-14 15:32:37 +0200

    update, fix error

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import CardReviewCarousel from "../components/CardReviewCarousel";
+import CardCastingCarousel from "../components/Casting/CardCastingCarousel";
 
 export default function Home() {
   return (
     <header className="App-header">
       <CardReviewCarousel movie={550} />
+      <CardCastingCarousel movie={550} />
     </header>
   );
 }
