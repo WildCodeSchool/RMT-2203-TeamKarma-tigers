59149770a5706c334f9a61d39a835d6007966e47
kaitolight <alexandre.pillias@gmail.com>
2022-04-14 12:05:45 +0200

    almost done with read more button

--- frontend/src/components/CardDetailSynopsis.jsx
+++ frontend/src/components/CardDetailSynopsis.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react/cjs/react.production.min";
 import "../styles/CardDetailSynopsis.css";
 
+function ReadMore({ children, maxcharactercount = 100 }) {
+  const text = children;
+  const [isTruncated, setIsTruncated] = useState(true);
+  const resultString = isTruncated ? text.slice(0, maxcharactercount) : text;
+
+  const toggleIsTruncated = () => {
+    setIsTruncated(!isTruncated);
+  };
+
+  return (
+    <p className="text-left">
+      {resultString}
+      <span onClick={toggleIsTruncated} aria-hidden="true">
+        {isTruncated ? "...read more" : " show less"}
+      </span>
+    </p>
+  );
+}
+
 export default function CardDetailSynopsis() {
-  return <div className="second-container" />;
+  return (
+    <div className="second-container">
+      <h3>SYNOPSIS</h3>
+      <ReadMore>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+        laboriosam quas facilis maxime quibusdam earum harum iusto, quisquam
+        aliquam voluptates praesentium ipsum debitis a nobis excepturi iure quia
+        placeat Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perspiciatis laudantium dignissimos sunt consequatur obcaecati ad, ipsam
+        aliquam aliquid culpa. Saepe dolorem cum laudantium eaque facilis
+        provident asperiores. Molestias, molestiae saepe.
+      </ReadMore>
+    </div>
+  );
 }
