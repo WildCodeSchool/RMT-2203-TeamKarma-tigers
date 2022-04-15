62d5a139f7b8e3a06a7683ae13dd70530a769a72
Julie Combeau <combeau.julie@gmail.com>
2022-04-15 10:51:55 +0200

    add url empty image

--- frontend/src/components/Casting/CardCasting.jsx
+++ frontend/src/components/Casting/CardCasting.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import emptyImage from "../../assets/emptyImage.svg";
 
 function CardCasting({ cast }) {
   return (
     <div>
       <div>
         <img
-          src={`https://image.tmdb.org/t/p/w200/${cast.profile_path}`}
+          src={
+            cast.profile_path
+              ? `https://image.tmdb.org/t/p/w200/${cast.profile_path}`
+              : `${emptyImage}`
+          }
           alt=""
+          width="200px"
+          backgroundColor="grey"
         />
         <div>
           <h4>{cast.name}</h4>
