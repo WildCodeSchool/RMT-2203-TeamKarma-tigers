f40b585078f6d5582488d615a06f572701de3db3
Julie Combeau <combeau.julie@gmail.com>
2022-04-23 17:15:35 +0200

    first commit on this new branch

--- frontend/src/components/CardReviewCarousel.jsx
+++ frontend/src/components/CardReviewCarousel.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import CardReview from "./CardReview";
 
 function CardReviewCarousel({ movie }) {
