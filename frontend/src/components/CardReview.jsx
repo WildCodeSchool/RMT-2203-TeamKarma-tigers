5cb7747166d6c35d2a8121eb2903155e9f033e35
Julie Combeau <combeau.julie@gmail.com>
2022-04-24 11:18:11 +0200

    design container, avatar, raiting

--- frontend/src/components/CardReview.jsx
+++ frontend/src/components/CardReview.jsx
@@ -1,31 +1,46 @@
 import React from "react";
-import emptyImage from "../assets/emptyImage.svg";
+import { Avatar, Container, Flex, Text, Box } from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
 
 function CardReview({ review }) {
   return (
-    <div>
-      <div>
-        <img
+    <Container border="1px" maxH="150px">
+      <Flex justifyContent="space-around">
+        <Avatar
+          size="md"
           src={
             review.author_details.avatar_path === null ||
             review.author_details.avatar_path.includes("gravatar") === true
-              ? `${emptyImage}`
+              ? `https://bit.ly/broken-link`
               : `https://image.tmdb.org/t/p/w500/${review.author_details.avatar_path}`
           }
           alt=""
-          width="200px"
-          backgroundColor="grey"
         />
-        <div>
+        <Flex flexDirection="column">
           <h4>{review.author}</h4>
           <p>{review.updated_at}</p>
-        </div>
-      </div>
-      <div>
-        <p>{review.author_details.rating}</p>
-        <p>{review.content}</p>
-      </div>
-    </div>
+        </Flex>
+      </Flex>
+      <Box display="flex" mt="2" alignItems="center">
+        {Array(5)
+          .fill("")
+          .map((_, i) => (
+            <StarIcon
+              w={4}
+              h={4}
+              key={review.id}
+              color={
+                i < Math.round(review.author_details.rating / 2)
+                  ? "teal.500"
+                  : "gray.300"
+              }
+            />
+          ))}
+      </Box>
+      <Box>
+        <Text isTruncated>{review.content}</Text>
+      </Box>
+    </Container>
   );
 }
 export default CardReview;
