c859a5fd1cc0c474e6e375e9ec364693ed9f391e
Julie Combeau <combeau.julie@gmail.com>
2022-04-24 11:45:18 +0200

    adjustment design rewiews

--- frontend/src/components/CardReview.jsx
+++ frontend/src/components/CardReview.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Avatar, Container, Flex, Text, Box } from "@chakra-ui/react";
 import { StarIcon } from "@chakra-ui/icons";
+import moment from "moment";
 
 function CardReview({ review }) {
   return (
-    <Container border="1px" maxH="150px">
+    <Container
+      border="1px"
+      maxH="200px"
+      bg="#282c34"
+      color="white"
+      margin="1rem"
+      padding="0.5rem"
+    >
       <Flex justifyContent="space-around">
         <Avatar
+          alignSelf="center"
           size="md"
           src={
             review.author_details.avatar_path === null ||
@@ -17,29 +26,31 @@ function CardReview({ review }) {
           alt=""
         />
         <Flex flexDirection="column">
-          <h4>{review.author}</h4>
-          <p>{review.updated_at}</p>
+          <Text>{review.author}</Text>
+          <Text mt={2}>{moment(review.updated_at).format("MMMM Do YYYY")}</Text>
         </Flex>
       </Flex>
-      <Box display="flex" mt="2" alignItems="center">
-        {Array(5)
-          .fill("")
-          .map((_, i) => (
-            <StarIcon
-              w={4}
-              h={4}
-              key={review.id}
-              color={
-                i < Math.round(review.author_details.rating / 2)
-                  ? "teal.500"
-                  : "gray.300"
-              }
-            />
-          ))}
-      </Box>
-      <Box>
-        <Text isTruncated>{review.content}</Text>
-      </Box>
+      <Flex flexDirection="column">
+        <Box display="flex" mt="2" alignSelf="center" margin="0.5rem">
+          {Array(5)
+            .fill("")
+            .map((_, i) => (
+              <StarIcon
+                w={4}
+                h={4}
+                key={review.id}
+                color={
+                  i < Math.round(review.author_details.rating / 2)
+                    ? "teal.500"
+                    : "gray.300"
+                }
+              />
+            ))}
+        </Box>
+        <Text noOfLines={3} fontSize="xs" margin="0.5rem" align="center">
+          {review.content}
+        </Text>
+      </Flex>
     </Container>
   );
 }
