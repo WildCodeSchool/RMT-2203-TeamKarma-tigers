15b8dc3bba1d5e724fedf3d7b7bbe2026a002ebf
Marc LANTOL <marc.lantol@gmail.com>
2022-04-27 13:40:44 +0200

    Add an false image poster in case of missing poster in api

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -2,6 +2,7 @@ import { React, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../styles/searchbar.css";
+import emptyImage from "../assets/emptyImage.svg";
 
 function Searchbar() {
   const [moviesData, setMoviesData] = useState([]);
@@ -36,10 +37,14 @@ function Searchbar() {
       {moviesData.length !== 0 && search !== "" && (
         <div className="movie-list">
           {moviesData.map((movie) => (
-            <a className="movie-link" href={`/movies/${movie.id}`}>
+            <a className="movie-link" href={`/movies/${movie.id} `}>
               <img
                 className="movie-img-searchbar"
-                src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+                src={
+                  movie.poster_path
+                    ? `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
+                    : `${emptyImage}`
+                }
                 alt="Movie poster"
               />
               {movie.title}
