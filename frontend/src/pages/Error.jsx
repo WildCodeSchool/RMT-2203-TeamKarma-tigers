e7445d7be7e4511e468fdf5bbe4ef6249bc7f270
Marc LANTOL <marc.lantol@gmail.com>
2022-05-01 09:46:36 +0200

    Add a background on error page & a 100 vh height

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -6,8 +7,10 @@ function Error() {
   return (
     <div>
       <Header isOnHome={false} />
-      <p>page 404</p>
-      {/* Importer movie carousel filtered by the query */}
+      <Box bg="#15141f" color="white" h="100vh">
+        <p>page 404</p>
+        {/* Importer movie carousel filtered by the query */}
+      </Box>
       <Footer />
     </div>
   );
