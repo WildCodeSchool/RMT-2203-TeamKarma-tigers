7cb2d6b4a7f382c570ecc658c2a3d7b9d45e7661
Marc LANTOL <marc.lantol@gmail.com>
2022-05-03 20:44:41 +0200

    Fix the header to be just position fixed on home page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,7 +16,12 @@ export default function Header({ isOnHome }) {
       align="center"
       justify="space-between"
       h="100px"
-      position="fixed"
+      bgGradient={
+        isOnHome
+          ? "linear-gradient(to bottom, rgba(0,2,10,0.8),transparent)"
+          : null
+      }
+      position={isOnHome ? "fixed" : "relative"}
     >
       <Flex gap={10} w="100%" align="center" justify="space-between">
         <a href="/">
