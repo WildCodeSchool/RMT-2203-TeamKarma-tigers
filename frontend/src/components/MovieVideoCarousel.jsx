aabea4bfd89d27eae1a8579614501f9f3fb9df1a
Robin.R <robin-renee@hotmail.fr>
2022-05-10 16:12:44 +0200

    finished responsive video

--- frontend/src/components/MovieVideoCarousel.jsx
+++ frontend/src/components/MovieVideoCarousel.jsx
@@ -25,19 +25,16 @@ export default function MovieVideoCarousel({ movie }) {
 
   const filterVideos = (cur, allVid) => {
     const res = [];
-    if (!allVid.length) return null;
+
     if (allVid.length < 3) return allVid;
     res.push(allVid[cur]);
 
     if (cur === allVid.length - 1) {
       res.push(allVid[0]);
-      res.push(allVid[1]);
     } else if (cur === allVid.length - 2) {
       res.push(allVid[allVid.length - 1]);
-      res.push(allVid[0]);
     } else {
       res.push(allVid[cur + 1]);
-      res.push(allVid[cur + 2]);
     }
     return res;
   };
@@ -72,10 +69,10 @@ export default function MovieVideoCarousel({ movie }) {
   return (
     <Flex
       h={{ base: "50vh", md: "40vh", xl: "30vh" }}
-      mr={{ "2xl": "18em" }}
+      mr={{ xl: "3em", "2xl": "18em" }}
       ml={{ xl: "5em" }}
       W="70vw"
-      mt={{ lg: "2.5em", xl: "5em", "2xl": "5em" }}
+      mt={{ base: "1.5em", lg: "2.5em", xl: "5em", "2xl": "5em" }}
       justify="center"
       flexDirection={{ base: "column", md: "row" }}
     >
@@ -83,14 +80,23 @@ export default function MovieVideoCarousel({ movie }) {
         {allVideos.length > 3 && (
           <Button
             alignSelf="center"
-            _hover={{ transform: "scale(1.2)", cursor: "pointer" }}
+            _hover={{ cursor: "pointer" }}
             variant="solid"
+            transform={{
+              base: "rotate(90deg)",
+              sm: "rotate(90deg)",
+              md: "rotate(0deg)",
+              lg: "rotate(0deg)",
+              xl: "rotate(0deg)",
+              "2xl": "rotate(0deg)",
+            }}
+            mb={{ base: "10px" }}
             borderRadius="100%"
             minWidth="3.125em"
             maxWidth="3.125em"
             minHeight="3.125em"
             maxHeight="3.125em"
-            mr={{ xl: "1em", "2xl": "1em" }}
+            mr={{ lg: "10px", xl: "1em", "2xl": "1em" }}
             ml={{ "2xl": "12em" }}
           >
             <ArrowBackIcon color="red" fontSize="3rem" onClick={prevSlide} />
@@ -104,14 +110,23 @@ export default function MovieVideoCarousel({ movie }) {
         {allVideos.length > 3 && (
           <Button
             alignSelf="center"
-            _hover={{ transform: "scale(1.2)", cursor: "pointer" }}
+            _hover={{ cursor: "pointer" }}
             variant="solid"
+            transform={{
+              base: "rotate(90deg)",
+              sm: "rotate(90deg)",
+              md: "rotate(0deg)",
+              lg: "rotate(0deg)",
+              xl: "rotate(0deg)",
+              "2xl": "rotate(0deg)",
+            }}
+            mt={{ base: "10px" }}
             borderRadius="100%"
             minWidth="3.125em"
             maxWidth="3.125em"
             minHeight="3.125em"
             maxHeight="3.125em"
-            ml={{ sm: "0.4em", md: "0.4em", lg: "0.4em", xl: "1.5em" }}
+            ml={{ sm: "0.4em", md: "0.4em", lg: "10px", xl: "1.5em" }}
           >
             <ArrowForwardIcon color="red" fontSize="3rem" onClick={nextSlide} />
           </Button>
