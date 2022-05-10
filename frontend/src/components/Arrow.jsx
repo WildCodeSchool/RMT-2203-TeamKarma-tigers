5091b32f845bbe170804e1037d13bc3ca5d083b9
Marc LANTOL <marc.lantol@gmail.com>
2022-05-10 15:03:04 +0200

    Fix height of trendings sentence

--- frontend/src/components/Arrow.jsx
+++ frontend/src/components/Arrow.jsx
@@ -1,3 +1,4 @@
+import { Heading } from "@chakra-ui/react";
 import React from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import "../styles/arrow.css";
@@ -6,7 +7,14 @@ function Arrow() {
   return (
     <div className="container">
       <AnchorLink href="#stuff">
-        <h1>Click me if you want to see the trending Movies of this week !</h1>
+        <Heading
+          id="trend-text"
+          fontSize={{ base: "20px", sm: "25px", md: "30px", lg: "40px" }}
+          ml="40px"
+          mr="40px"
+        >
+          Click me if you want to see the trending Movies of this week !
+        </Heading>
       </AnchorLink>
     </div>
   );
