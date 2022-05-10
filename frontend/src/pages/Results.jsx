92f226fe208873c2ded2c5c132e9b1833a4a9456
Julie Combeau <combeau.julie@gmail.com>
2022-05-10 10:54:36 +0200

    size image page no result

--- frontend/src/pages/Results.jsx
+++ frontend/src/pages/Results.jsx
@@ -20,7 +20,7 @@ function Results() {
   const [numOfResults, setNumOfResults] = useState(null);
 
   return (
-    <Box bg="#15141f">
+    <Box bg="#15141f" overflowX="hidden">
       <Header isOnHome={false} />
       <VStack gap={30} pt={10}>
         <Heading as="h1" color="white">
@@ -42,34 +42,54 @@ function Results() {
           <Flex
             justify="center"
             direction="column"
-            p="10vh"
+            p={{ base: "1rem", md: "3rem" }}
             align="flex-start"
-            h="80vh"
             bgImage={emptyResults}
             w="100vw"
+            overflowX=""
+            h={{ base: "60vh", "2xl": "90vh" }}
             filter="auto"
+            bgRepeat="no-repeat"
+            bgSize={{ base: "cover", "2xl": "110% 100%" }}
+            bgPosition="center"
           >
             <Heading
               as="h2"
-              fontSize="60px"
+              fontSize={{ base: "2xl", md: "60px" }}
               color="white"
-              maxW="55%"
+              maxW={{ base: "100%", md: "55%" }}
               align="left"
             >
               Oops, it seems we could not find anything for :{search}
             </Heading>
-            <Text fontSize="2xl" color="white" align="left">
+            <Text
+              fontSize={{ base: "xl", md: "60px" }}
+              color="white"
+              align="left"
+            >
               There are no movies that matched your query.
             </Text>
-            <Text fontSize="2xl" color="white" align="left">
+            <Text
+              fontSize={{ base: "xl", md: "60px" }}
+              color="white"
+              align="left"
+            >
               Check for any potential mispellings or tap into :
             </Text>
 
             <UnorderedList>
-              <ListItem fontSize="2xl" color="white" align="left">
+              <ListItem
+                fontSize={{ base: "xl", md: "60px" }}
+                color="white"
+                align="left"
+              >
                 All movies
               </ListItem>
-              <ListItem fontSize="2xl" color="white" align="left">
+              <ListItem
+                fontSize={{ base: "xl", md: "60px" }}
+                color="white"
+                align="left"
+              >
                 Popular Movies
               </ListItem>
             </UnorderedList>
